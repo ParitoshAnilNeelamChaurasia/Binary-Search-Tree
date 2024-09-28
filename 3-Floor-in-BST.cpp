e56e0e6bb7247651ff3263a463cb0118d0d79259
ParitoshAnilNeelamChaurasia <149140841+ParitoshAnilNeelamChaurasia@users.noreply.github.com>
2024-09-29 01:33:46 +0530

    Create 3-Floor-in-BST.cpp

---
+++ 3-Floor-in-BST.cpp
@@ -0,0 +1,88 @@
+// https://www.geeksforgeeks.org/problems/implementing-ceil-in-bst/1
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// Function to search a node in BST.
+class Solution{
+
+public:
+    int floor(Node* root, int x) {
+        
+        int flor = -1 ;
+        
+        while(root != NULL)
+        {
+            if(root->data == x)
+            {
+                flor = root->data ;
+                return flor ;
+            }
+            
+            if(x > root->data)
+            {
+                flor = root->data ;
+                root = root->right ;
+            }
+            else
+            {
+                root = root->left ;
+            }
+        }
+        return flor ;
+    }
+};
+
+//{ Driver Code Starts.
+
+Node *insert(Node *tree, int val) {
+    Node *temp = NULL;
+    if (tree == NULL) return new Node(val);
+
+    if (val < tree->data) {
+        tree->left = insert(tree->left, val);
+    } else if (val > tree->data) {
+        tree->right = insert(tree->right, val);
+    }
+
+    return tree;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        Node *root = NULL;
+
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            int k;
+            cin >> k;
+            root = insert(root, k);
+        }
+
+        int s;
+        cin >> s;
+        Solution obj;
+        cout << obj.floor(root, s) << "\n";
+    }
+}
+
+// } Driver Code Ends
